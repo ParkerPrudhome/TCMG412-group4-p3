f555fef7b625814988389aba467da89821e92d37
Shrubrio <90871281+Shrubrio@users.noreply.github.com>
2022-09-20 14:49:17 -0500

    updated file
    removed main() to simplify code, added request total and last six variables to satisfy project requirements.
    realized lines are != requests, left len(fp.readlines()) just to output total lines.
    has been tested in google linux VM.

--- log file parser.py
+++ log file parser.py
@@ -1,35 +1,38 @@
+from stat import filemode
 import urllib.request
 from urllib.request import urlretrieve
 import os.path
 from os import path
 
-def main():
-    weburl = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
-    resultcode = print("result code: " + str(weburl.getcode()))
+
+weburl = urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log")
+resultcode = print("result code: " + str(weburl.getcode()))
     
-    if path.isfile('logfile.txt') == False:
-        print('downloading log file...')
-        url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-        urllib.request.urlretrieve (url, 'logfile.txt')
-        print('done')
-    else: print('this file is already present on your workstation')
-    
+if path.isfile('logfile.txt') == False:
+    print('downloading log file...')
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    urllib.request.urlretrieve (url, 'logfile.txt')
+    print('done')
+else: print('this file is already present on your workstation')
+
+lastsixmonths = ["May/1995:", "Jun/1995:" , "Jul/1995:", "Aug/1995:", "Sep/1995:", "Oct/1995:"]  
+lastsix = 0
+request_total = 0
+
 with open(r"logfile.txt", 'r') as fp:
     lines = len(fp.readlines())
-    print('Total number of logs in file between Oct 1994 and Oct 1995:', lines)
+    print('Total number of lines in file between Oct 1994 and Oct 1995:', lines)
     
-#now to find # of logs in first 6 months
-#not currently working for some reason
-#start = 0
-#date = "11/Apr/1995"
-#for line in lines:
-    #start += 1
-    #if date in line:
-        #break
-    
-#lastsix = lines - start + 1
+for line in open(r"logfile.txt", 'r'):
+    if "GET" in line:
+        request_total += 1
+        
+        for month in lastsixmonths:
+            if month in line:
+                lastsix +=1
 
-#print("There is" , lastsix ,"logs in the six months")
+print("total number of requests:" , request_total)
+
+print("The number of logs in the last six months is", lastsix)
     
-if __name__ == "__main__":
-    main()
+fp.close()
